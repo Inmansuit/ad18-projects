915b54c4e12e2c4f6065eac6f6d28880775d2cb2
Diana Morozova <inmansuit@gmail.com>
2019-06-16 09:26:39 +0300

    Space program

---
+++ gone/Assets/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 600f;
+
+    float movement = 0f;
+
+    public Text countText;
+
+    private int count;
+
+    public Text winText;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Scoring"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+  
+
+
+void SetCountText()
+    {
+        countText.text = "Count: " + countText.ToString();
+        if ( count >= 5)
+        {
+        winText.text = "You win!";
+        }
+    }
+
+ 
+}
