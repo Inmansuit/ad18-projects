ff698690be54bf3a1dcc20c80df235a250c0957b
Diana Morozova <inmansuit@gmail.com>
2019-06-18 23:44:47 +0300

    Updated and runable version of the game
    Bugs fixed. Game transferred to a 2D format project

---
+++ Defender.Game/Assets/Scripts/Scoring.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+
+
+    public float spawnRate = 1f;
+
+    public GameObject ScorePrefab;
+
+    private float nextScore = 0f;
+
+    void Update()
+        {
+            if (Time.time >= nextScore)
+            {
+                Instantiate(ScorePrefab, Vector3.zero, Quaternion.identity);
+                nextScore = Time.time + 1f / spawnRate;
+            }
+        }
+    
+}
